e7f293d0249890f10061e31d59267591c41d1add
Dcbaas <bcdave.301@gmail.com>
2018-12-13 20:42:21 -0500

    Init commit

---
+++ LeetCodePractice/Best Time to Sell Stocks/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Best_Time_to_Sell_Stocks
+{
+    class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+            List<ProfitData> data = new List<ProfitData>();
+            for (int index = 0; index < prices.Length; ++index)
+            {
+                ProfitData profit = new ProfitData() {DaySold = prices[-index], TotalProfit = 0};
+
+            }
+            return maxProfit;
+        }
+    }
+
+    internal class ProfitData
+    {
+        private int totalProfit;
+        private int daySold;
+
+        public int TotalProfit { get => totalProfit; set => totalProfit = value; }
+        public int DaySold { get => daySold; set => daySold = value; }
+    }
+}
