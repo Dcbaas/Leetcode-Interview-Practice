f8b561bb300cc09059b277f05ef2696756812eb3
Dcbaas <bcdave.301@gmail.com>
2018-12-15 14:14:29 -0500

    Finished rotate

--- LeetCodePractice/Rotate Array/SolutionTester.cs
+++ LeetCodePractice/Rotate Array/SolutionTester.cs
@@ -14,11 +14,24 @@ namespace LeetCodePractice.Rotate_Array
 
             int[] test = new[] {1, 2, 3, 4, 5, 6, 7};
             var k = 3;
-
+//            PrintList(ref test);
             solution.Rotate(test, k);
 
+            
+            PrintList(ref test);
 
 
+        }
+
+        private static void PrintList(ref int[] nums)
+        {
+            foreach (var number in nums)
+            {
+                Console.Write(number +", ");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
         }
     }
 }
