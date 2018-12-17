16ee549d3aa6ce7bd96564a9dc3005961c478d7a
Dcbaas <bcdave.301@gmail.com>
2018-12-16 23:16:57 -0500

    Finished Single number problem

---
+++ LeetCodePractice/Single Number/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Single_Number
+{
+    class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            try
+            {
+                for (int index = 0; index < nums.Length; index++)
+                {
+                    if (nums[index] != nums[index + 1])
+                    {
+                        return nums[index];
+                    }
+                    ++index;
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+            }
+            
+
+            return nums[nums.Length - 1];
+        }
+    }
+}
