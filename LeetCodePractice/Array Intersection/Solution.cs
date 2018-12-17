6b312b7884fe9a539bc9113962ae3214a8b2f51a
Dcbaas <bcdave.301@gmail.com>
2018-12-17 11:51:01 -0500

    Unfinished intersection Problem

---
+++ LeetCodePractice/Array Intersection/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Array_Intersection
+{
+    class Solution
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            List<int> intersection = new List<int>();
+            Array.Sort(nums1);
+            Array.Sort(nums2);
+        }
+    }
+}
